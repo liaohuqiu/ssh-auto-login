1b91acf09671de2a0b166201812ea3cdfdcfa5a8
Huqiu Liao <liaohuqiu@gmail.com>
2013-08-21 19:53:33 -0700

    add to command

--- install_shortcut.sh
+++ install_shortcut.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-if [ ! $# -eq 2 ]; then
-    echo "Usage: sh $0 user_name password"
-    echo "exsample: sh $0 huqiu Flzx3qc"
+if [ ! $# -eq 4 ]; then
+    echo "Usage: sh $0 alias_key host_name user_name password"
+    echo "exsample: sh $0 to_pre host_name huqiu Flzx3qc"
     exit;
 fi
 
@@ -11,28 +11,20 @@ fi
 
 init
 
-user_name=$1
-password=$2
+alias_key=$1
+host=$2
+user_name=$3
+password=$4
 
-alias_key='to'
 work_script_file_path="$auto_gen_dir/$alias_key.sh"
 
-#build script
 cat >$work_script_file_path  <<EOF
 #!/bin/bash
-
-if [ ! \$# -eq 1 ]; then
-    echo "Usage: to hostname"
-    exit;
-fi
-
-host=\$1
+host="$host"
 user_name="$user_name"
 password="$password"
 cd $current_dir/files
 ./auto_login.sh \$host \$user_name \$password
 EOF
 chmod u+x $work_script_file_path
-
 set_alias $alias_key $work_script_file_path
-
