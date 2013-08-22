8fdef39516c5ee9884c8165284ed24af3b3b9921
Huqiu Liao <liaohuqiu@gmail.com>
2013-08-21 19:16:29 -0700

    make it work

--- files/auto_login.sh
+++ files/auto_login.sh
@@ -6,12 +6,5 @@ file=$HOME"/.ssh/master-$user@$host:22"
 if [ -e "$file" ]; then
     ssh $user@$host
 else
-    known_hosts=$HOME'/.ssh/known_hosts'
-    save_RSA_key=''
-    if [ -f $known_hosts ]; then
-        if ! grep -q "$host" $known_hosts; then
-            save_RSA_key='yes'
-        fi
-    fi
     ./auto_login.exp $host $user $password $save_RSA_key
 fi
--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #
-if [ ! $# -eq 5 ]; then
+shopt -s expand_aliases
+
+if [ ! $# -eq 4 ]; then
     echo "Usage: sh $0 short_cut_command host_name user_name password"
     echo "exsample: sh $0 to_pre host_name huqiu Flzx3qc"
     exit;
 fi
-if [ ! -d ~/.ssh ]; then
-    mkdir ~/.ssh
-fi
+
+bashrc=$HOME/.bashrc
 current_dir=`pwd`
 auto_gen_dir="$current_dir/auto_gen"
+if [ ! -d $HOME"/.ssh" ]; then
+    mkdir $HOME"/.ssh"
+fi
+if [ ! -d $auto_gen_dir ]; then
+    mkdir $auto_gen_dir
+fi
+
+#todo: meger ssh_config
 cp -f files/ssh_config ~/.ssh/ssh_config
 chmod 400 ~/.ssh/ssh_config
 
@@ -19,17 +28,20 @@ user_name=$3
 password=$4
 pre_passcode=$5
 
-cat > "$auto_gen_dir/$command.sh" <<EOF
+auto_login_file_path="$auto_gen_dir/$command.sh"
+
+cat >$auto_login_file_path  <<EOF
 #!/bin/bash
 host="$ip"
 user_name="$user_name"
 password="$password"
-prePASSCODE="$pre_passcode"
 cd $current_dir/files
-./auto_login.sh \$host \$user_name \$password \$prePASSCODE
+./auto_login.sh \$host \$user_name \$password
 EOF
-chmod u+x $command.sh
+chmod u+x $auto_login_file_path
 
-if ! grep -q "alias $command" ~/.bashrc; then
-    echo alias $command="$auto_gen_dir/$command.sh" >> ~/.bashrc
+#todo:  make alias take effect instantly
+if ! grep -q "alias $command" $bashrc; then
+    cmd="alias $command=$auto_gen_dir/$command.sh"
+    echo $cmd >> $bashrc
 fi
